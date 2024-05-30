6a3b581ff40f49bf65f2e0cb64df675960aa6703
Tiago Ávila <tiago.avila09@gmail.com>
2024-05-30 09:52:50 -0300

    created project

---
+++ rinha_2024/lib/rinha2024.ex
@@ -0,0 +1,18 @@
+defmodule Rinha2024 do
+  @moduledoc """
+  Documentation for `Rinha2024`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rinha2024.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ rinha_2024/test/rinha2024_test.exs
@@ -0,0 +1,8 @@
+defmodule Rinha2024Test do
+  use ExUnit.Case
+  doctest Rinha2024
+
+  test "greets the world" do
+    assert Rinha2024.hello() == :world
+  end
+end

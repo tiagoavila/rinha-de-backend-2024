bc2fcb7f20f672bc33e1774242392dbd3d55a891
Tiago Ávila <tiago.avila09@gmail.com>
2024-05-31 10:21:44 -0300

    Added missing files

---
+++ rinha_2024/lib/elli_callback.ex
@@ -0,0 +1,13 @@
+defmodule ElliCallback do
+    @behaviour :elli_handler
+    
+    @impl true
+    def handle(req, _args) do
+        do_handle(:elli_request.method(req), :elli_request.path(req), req)
+    end
+    
+    defp do_handle(:GET, [], _req), do: {:ok, "Welcome! I'm running on Docker. I'm awesome!!!"}
+    
+    @impl true
+    def handle_event(_event, _data, _args), do: :ok
+end
---
+++ rinha_2024/lib/rinha/application.ex
@@ -0,0 +1,26 @@
+defmodule Rinha.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+  
+  alias ElliCallback
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Rinha.Worker.start_link(arg)
+      # {Rinha.Worker, arg}
+      %{
+        id: :elli,
+        start: {:elli, :start_link, [ [ callback: ElliCallback, port: 3000 ]]}
+      },
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Rinha.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

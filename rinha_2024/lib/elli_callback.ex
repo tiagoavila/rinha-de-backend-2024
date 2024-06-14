782a51f7ad903462d4cb418cf5b54724cf5466a6
Tiago Ávila <tiago.avila09@gmail.com>
2024-06-14 11:45:29 -0300

    Implemented validation

--- rinha_2024/lib/elli_callback.ex
+++ rinha_2024/lib/elli_callback.ex
@@ -1,6 +1,8 @@
 defmodule ElliCallback do
   @behaviour :elli_handler
 
+  alias Rinha.Customer
+
   @impl true
   def handle(req, _args) do
     do_handle(:elli_request.method(req), :elli_request.path(req), req)
@@ -8,7 +10,15 @@ defmodule ElliCallback do
 
   defp do_handle(:GET, [], _req), do: {:ok, "Welcome! I'm running on Docker. I'm awesome!!!"}
 
-  defp do_handle(:POST, ["clientes", id, "transacoes"], req), do: {:ok, "post to id #{id}"}
+  defp do_handle(:POST, ["clientes", customer_id, "transacoes"], req) do 
+    customer_id = String.to_integer(customer_id)
+    validation_result = :elli_request.body(req) |> Customer.handle_transaction(customer_id) |> IO.inspect(label: "validation_result") 
+    case validation_result do
+      {:error, message} -> {400, message}
+      {:client_not_found, message} -> {404, message}
+      {:ok, transaction} -> {:ok, "post to id #{customer_id}"} 
+    end
+  end
 
   @impl true
   def handle_event(_event, _data, _args), do: :ok
--- rinha_2024/lib/rinha/customer.ex
+++ rinha_2024/lib/rinha/customer.ex
@@ -1,9 +1,41 @@
 defmodule Rinha.Customer do
   use Ecto.Schema
 
+  @customer_ids MapSet.new([1, 2, 3, 4, 5])
+
   @primary_key {:id, :integer, autogenerate: false}
   schema "cliente" do
     field(:limite, :integer)
     field(:saldo, :integer)
+  end
+
+  def handle_transaction(transaction_body, customer_id) do
+    Poison.decode!(transaction_body) 
+    |> validate_required_fields()
+    |> validate_transaction(customer_id)
+  end
+
+  defp validate_required_fields(transaction) do
+    cond do
+      Map.has_key?(transaction, "descricao") && Map.has_key?(transaction, "tipo") && Map.has_key?(transaction, "valor") -> transaction
+      true -> {:error, "descricao, tipo e valor são campos obrigatórios"}
+    end
+  end
+
+  defp validate_transaction({:error, _} = required_fields_validation_result, _), do: required_fields_validation_result
+
+  defp validate_transaction(%{"descricao" => description, "tipo" => type, "valor" => value} = transaction, customer_id) do
+    cond do
+      type != "c" && type != "d" -> {:error, "Tipo de transação inválido"}
+      value <= 0 || value |> Integer.to_string() |> String.slice(-2..-1) != "00" -> {:error, "Valor deve ser um número inteiro positivo"}
+      description |> validate_description_length() -> {:error, "Descrição deve ter entre 1 e 10 caracteres"}
+      !MapSet.member?(@customer_ids, customer_id) -> {:client_not_found, "Cliente não encontrado"}
+      true -> {:ok, transaction}
+    end
+  end
+
+  defp validate_description_length(description) do
+    length = String.length(description)
+    length < 1 || length > 10
   end
 end
